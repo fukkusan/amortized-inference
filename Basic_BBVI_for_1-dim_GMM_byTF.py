c124bc1357736a1c4e52c06e836f2f5b718d37ab
USER <user@USERnoMacBook-Air.local>
2017-10-30 15:32:14 +0900

    lambda_piの値が負にならないように対処

--- Basic_BBVI_for_1-dim_GMM_byTF.py
+++ Basic_BBVI_for_1-dim_GMM_byTF.py
@@ -34,7 +34,7 @@ class BasicBBVI():
         
         # Variational parameters
         self.lambda_pi = tf.Variable(tf.ones([self.K])/self.K, dtype=tf.float32, trainable=True, name='lambda_pi')
-        self.lambda_mu = tf.Variable(tf.truncated_normal([self.D, self.K], mean = 0.0, stddev=1.0), dtype=tf.float32, trainable=True, name='lambda_mu')
+        self.lambda_mu = tf.Variable(tf.truncated_normal([self.D, self.K], mean = 0.0, stddev=2.0), dtype=tf.float32, trainable=True, name='lambda_mu')
         self.lambda_z = tf.Variable(tf.ones([self.N, self.K])/self.K, dtype=tf.float32, trainable=True, name='lambda_z')
         
         
@@ -48,12 +48,37 @@ class BasicBBVI():
         
         # Save previous variational parameters
         self.prev_lambda_pi = tf.ones(self.K)/self.K
-        self.prev_lambda_mu = tf.truncated_normal([self.D, self.K], mean = 0.0, stddev=1.0)
+        self.prev_lambda_mu = tf.truncated_normal([self.D, self.K], mean = 0.0, stddev=2.0)
         self.prev_lambda_z = tf.ones([self.N, self.K])/self.K
         
         
         
-    # Fit(Training) parameters
+    # Update distributions
+    def DistributionsUpdater(self):
+        # Variational approximation model
+        self.q_pi = tf.contrib.distributions.Dirichlet(self.lambda_pi)					# sample_shape=[self.K]
+        self.q_mu = tf.contrib.distributions.Normal(self.lambda_mu, tf.ones(self.K))	# sample_shape=[self.D, self.K]
+        self.q_z = tf.contrib.distributions.OneHotCategorical(self.lambda_z)					# sampe_shape=[self.N]
+        
+        
+        # Generative model
+        self.p_pi = tf.contrib.distributions.Dirichlet(self.gamma)						# sample_shape=[self.K]
+        self.p_mu = tf.contrib.distributions.Normal(self.alpha_mean, self.alpha_var)	# sample_shape=[self.D, self.K]
+        if self.update_counter > 0:
+            self.pi_gene = self.q_pi.sample(sample_shape=[1])[0]
+            self.mu_gene = self.q_mu.sample(sample_shape=[1])[0]
+        else :
+            self.pi_gene = self.p_pi.sample(sample_shape=[1])[0]
+            self.mu_gene = self.p_mu.sample(sample_shape=[1])[0]
+        self.p_z = tf.contrib.distributions.OneHotCategorical( self.pi_gene )					# sample_shape=[self.N, self.K]
+        self.generative_gauss = tf.contrib.distributions.Normal(self.mu_gene, tf.ones(self.K))
+        self.log_gene_gauss = self.generative_gauss.log_prob(self.generative_gauss.sample(sample_shape=[self.N]))
+        #self.logpx = tf.reduce_sum( tf.multiply( tf.to_float( self.p_z.sample(sample_shape=[self.N, self.K]) ), self.log_gene_gauss ), axis=1 )		#sample_shape=[self.N, self.K]
+        self.logpx = tf.reduce_sum( tf.multiply( tf.to_float( self.p_z.sample(sample_shape=[self.K]) ), self.log_gene_gauss ), axis=1 )		#sample_shape=[self.N, self.K]
+        
+        
+        
+        # Fit(Training) parameters
     def Fit(self, x_train):
         # Initiaize seesion
         init = tf.global_variables_initializer()
@@ -65,10 +90,10 @@ class BasicBBVI():
         vps_mu_1 = np.array(np.zeros(self.N//self.S), dtype=np.float32)
         vps_mu_2 = np.array(np.zeros(self.N//self.S), dtype=np.float32)
         vps_mu_3 = np.array(np.zeros(self.N//self.S), dtype=np.float32)
-        vps_loss = np.array(np.zeros(self.N//self.S), dtype=np.float32)
         df = pd.DataFrame(index=[], columns=['class1', 'class2', 'class3'])
         for epoch in range(self.N//self.S):
             vps = self.VariationalParametersUpdater(rho = 0.1)
+            vps = self.Care()
             variational_parameters = sess.run(vps, feed_dict = {
                 self.x: x_train
             })
@@ -78,10 +103,10 @@ class BasicBBVI():
             vps_mu_1[epoch] = variational_parameters[1][0][0]
             vps_mu_2[epoch] = variational_parameters[1][0][1]
             vps_mu_3[epoch] = variational_parameters[1][0][2]
-            q_mu_0 = np.array(np.zeros(50), dtype=np.float32)
-            q_mu_1 = np.array(np.zeros(50), dtype=np.float32)
-            q_mu_2 = np.array(np.zeros(50), dtype=np.float32)
-            x = np.array(np.zeros(50), dtype=np.float32)
+            q_mu_0 = np.array(np.zeros(20), dtype=np.float32)
+            q_mu_1 = np.array(np.zeros(20), dtype=np.float32)
+            q_mu_2 = np.array(np.zeros(20), dtype=np.float32)
+            x = np.array(np.zeros(20), dtype=np.float32)
             norm1 = mlab.normpdf(x_train, 0.0, 1.0)
             norm2 = mlab.normpdf(x_train, 2.0, 1.0)
             norm3 = mlab.normpdf(x_train, -2.0, 1.0)
@@ -89,7 +114,7 @@ class BasicBBVI():
             log_q = np.array(np.zeros(10), dtype=np.float32)
             epochs = np.array(np.zeros(10), dtype=np.float32)
             epochs[epoch] = epoch
-            for i in range(50):
+            for i in range(20):
                 x_element = -5.0 + i/10*5
                 x[i] = x_element
                 q_mu_0[i] =  self.q_mu.prob( x_element ).eval(session=sess)[0][0] 
@@ -111,6 +136,10 @@ class BasicBBVI():
             plt.title("q(mu|lambda_mu)")
             plt.legend()
             plt.show()
+            del q_mu_0
+            del q_mu_1
+            del q_mu_2
+            del x
             plt.clf()
             plt.close()
             se = pd.Series([vps_mu_1[epoch], vps_mu_2[epoch], vps_mu_3[epoch]], index=df.columns)
@@ -138,18 +167,15 @@ class BasicBBVI():
         self.log_q_z = list( np.ones(self.S, dtype=np.float32) )
         
         
-        
         while(s < self.S):
             # Update distributions
             if(self.update_counter>0):
                 self.DistributionsUpdater()
             
             self.log_p_x[s] = tf.reduce_sum( self.logpx )
-            #self.log_p_z[s] = tf.reduce_sum( self.p_z.log_prob(self.p_z.sample(sample_shape=[1]))[0] )
             self.log_p_mu[s] = tf.reduce_sum( self.p_mu.log_prob(self.p_mu.sample(sample_shape=[1]))[0][0] )
             self.log_p_pi[s] = self.p_pi.log_prob(self.p_pi.sample(sample_shape=[1]))[0]
             
-            #self.log_dirichlet = self.q_pi.log_prob(self.q_pi.sample(sample_shape=[self.K]))[0]
             self.log_dirichlet = self.q_pi.log_prob(self.q_pi.sample(sample_shape=[1]))[0]
             self.log_categorical = self.q_z.log_prob(self.q_z.sample(sample_shape=[1]))[0]
             self.log_gauss = self.q_mu.log_prob(self.q_mu.sample(sample_shape=[1]))[0]
@@ -158,6 +184,7 @@ class BasicBBVI():
             self.log_q_z[s] = tf.reduce_sum( self.log_categorical )
             
             s = s + 1
+        
         
         
         self.log_p = tf.add( tf.add( tf.add( tf.convert_to_tensor(self.log_p_x), tf.convert_to_tensor(self.log_p_z) ), tf.convert_to_tensor(self.log_p_pi) ), tf.convert_to_tensor(self.log_p_mu) ) 
@@ -186,18 +213,38 @@ class BasicBBVI():
         
         # Update variational parameters
         self.lambda_pi = tf.add(self.lambda_pi, tf.multiply(rho, self.sample_mean_pi))
-        #self.lambda_pi = tf.cond( tf.logical_or( tf.greater( tf.reduce_sum(self.lambda_pi), 10.00 ), tf.less( tf.reduce_sum(self.lambda_pi), 0.0 ) ), lambda: self.ConstraintMethod( self.lambda_pi ), lambda: self.lambda_pi )
-        self.lambda_pi = tf.cond( tf.less_equal( tf.reduce_sum(self.lambda_pi), 0.0 ), lambda: tf.abs( self.lambda_pi ), lambda: self.lambda_pi )
-        
         self.lambda_mu = tf.add(self.lambda_mu, tf.multiply(rho, self.sample_mean_mu))
-        #self.lambda_mu = tf.cond( tf.logical_or( tf.greater( tf.reduce_sum(self.lambda_mu[0]), 1.00 ), tf.less( tf.reduce_sum(self.lambda_mu[0]), -1.00 ) ), lambda: self.prev_lambda_mu, lambda: self.lambda_mu )
-        
         self.lambda_z = tf.add(self.lambda_z, tf.multiply(rho, self.sample_mean_z))
+        
+        
+        self.update_counter = self.update_counter + 1
+        
+        
+        return [self.lambda_pi, self.lambda_mu, self.lambda_z]
+    
+    
+    
+    # Care Values
+    def Care(self):
+        lambda_pi = []
+        lambda_pi.append( tf.split(self.lambda_pi, self.K, 0) )
+        k=0
+        while(k < self.K):
+            lambda_pi[0][k] = tf.cond( tf.less_equal( lambda_pi[0][k][0], 0.0 ), lambda: tf.abs( lambda_pi[0][k] ), lambda: lambda_pi[0][k] )
+            k = k + 1
+        if(k == self.K):
+            self.lambda_pi = tf.concat(lambda_pi[0], 0)
+        
+        
         lambda_z = []
         lambda_z.append( tf.split(self.lambda_z, self.N, 0) )
         n=0
         while(n < self.N):
-            #lambda_z[0][n] = tf.cond( tf.logical_or( tf.greater( tf.reduce_sum( lambda_z[0][n] ), 1.00000001 ), tf.less( tf.reduce_sum( lambda_z[0][n] ), 0.99999999 ) ), lambda: self.ConstraintMethod( lambda_z[0][n] ), lambda: lambda_z[0][n] )
+            k=0
+            while(k < self.K):
+                lambda_z[0][n] = tf.cond( tf.less_equal( lambda_z[0][n][0][k], 0.0 ), lambda: tf.abs(lambda_z[0][n]), lambda: lambda_z[0][n] )
+                k = k + 1
+            #lambda_z[0][n] = tf.abs( lambda_z[0][n] )
             lambda_z[0][n] = tf.cond( tf.not_equal( tf.reduce_sum( lambda_z[0][n] ), 1.0 ), lambda: self.ConstraintMethod( lambda_z[0][n] ), lambda: lambda_z[0][n] )
             n = n + 1
         if(n == self.N):
@@ -220,47 +267,17 @@ class BasicBBVI():
         self.prev_lambda_z = self.lambda_z
         
         
-        self.update_counter = self.update_counter + 1
-        
-        
         return [self.lambda_pi, self.lambda_mu, self.lambda_z]
-    
-    
-    
-    # Update distributions
-    def DistributionsUpdater(self):
-        # Variational approximation model
-        self.q_pi = tf.contrib.distributions.Dirichlet(self.lambda_pi)					# sample_shape=[self.K]
-        self.q_mu = tf.contrib.distributions.Normal(self.lambda_mu, tf.ones(self.K))	# sample_shape=[self.D, self.K]
-        self.q_z = tf.contrib.distributions.Categorical(self.lambda_z)					# sampe_shape=[self.N]
-        
-        
-        # Generative model
-        self.p_pi = tf.contrib.distributions.Dirichlet(self.gamma)						# sample_shape=[self.K]
-        self.p_mu = tf.contrib.distributions.Normal(self.alpha_mean, self.alpha_var)	# sample_shape=[self.D, self.K]
-        if self.update_counter > 0:
-            self.pi_gene = self.q_pi.sample(sample_shape=[1])[0]
-            #self.mu_gene = self.q_mu.sample(sample_shape=[1])[0][0]
-            self.mu_gene = self.q_mu.sample(sample_shape=[1])[0]
-        else :
-            self.pi_gene = self.p_pi.sample(sample_shape=[1])[0]
-            #self.mu_gene = self.p_mu.sample(sample_shape=[1])[0][0]
-            self.mu_gene = self.p_mu.sample(sample_shape=[1])[0]
-        self.p_z = tf.contrib.distributions.Categorical( self.pi_gene )					# sample_shape=[self.N, self.K]
-        self.generative_gauss = tf.contrib.distributions.Normal(self.mu_gene, tf.ones(self.K))
-        self.log_gene_gauss = self.generative_gauss.log_prob(self.generative_gauss.sample(sample_shape=[self.N]))
-        self.logpx = tf.reduce_sum( tf.multiply( tf.to_float( self.p_z.sample(sample_shape=[self.N, self.K]) ), self.log_gene_gauss ), axis=1 )		#sample_shape=[self.N]
     
     
     
     # Constraint Method
     def ConstraintMethod(self, vector_subspace):
-        element_sum = tf.reduce_sum( tf.abs(vector_subspace) )
-        vector_constrainted = tf.divide( tf.abs(vector_subspace), element_sum )
+        element_sum = tf.reduce_sum( vector_subspace )
+        vector_constrainted = tf.divide( vector_subspace, element_sum )
         
         
         return vector_constrainted
-    
     
     
     
